ec4492e6d2778f42850e00db779659990414a664
Alex <alex.cooper@docusign.com>
2018-06-04 16:09:08 -0700

    comment unused variables

--- test/OAuthClientTests.js
+++ test/OAuthClientTests.js
@@ -3,9 +3,10 @@ const docusign = require('../src/index');
 const apiClient = new docusign.ApiClient();
 
 const app = express();
+*/
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || 'localhost';
-
+/*
 const integratorKey = 'ae30ea4e-3959-4d1c-b867-fcb57d2dc4df'; // An IK for a non-mobile docusign account
 const integratorKeyImplicit = '68c1711f-8b19-47b1-888f-b49b4211d831'; // an IK with mobile selected in Docusign admin
 const clientSecret = 'b4dccdbe-232f-46cc-96c5-b2f0f7448f8f';
